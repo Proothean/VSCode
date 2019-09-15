ad88d8d3814a01c971faf1541d2b1b3c1d80842d
Proothean <prothean916@gmail.com>
2019-09-15 15:12:03 +0800

    add C++

--- C++/tutorial10.cpp
+++ C++/tutorial10.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 int main()
 {
     std::ofstream file;
-    file.open("hello.txt");
+    //file.open("hello.txt");
+    file.open("hello.txt",std::ios::app);
+    if(file.is_open())
+    {
+    std::cout<<"hey"<<std::endl;
+    }
+    std::vector<std::string> names;
+    names.push_back("Caleb");
+    names.push_back("Amy");
+    names.push_back("Susan");
+
+    for(std::string name : names)
+    {
+        file<<name<<std::endl;
+    }
+
     file.close();
 }
