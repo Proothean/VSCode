21b129b2b6bd352697c93b4c35bc3c66d32a1aad
Proothean <prothean916@gmail.com>
2019-09-18 13:28:07 +0800

    add NeuralNetwork

---
+++ NeuralNetwork/tutorial1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char **argv)
+{
+    //Input
+    Neuron *n1 = new Neuron(0.9);
+    Neuron *n2 = new Neuron(0.5);
+    Neuron *n2 = new Neuron(0.1);
+
+    //Output
+    Neuron *outputNeuron = new Neuron(0.0);
+
+    return 0;
+}
